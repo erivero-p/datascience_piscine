25772728df73486d8208eb5dfee7540ab970458b
Elisa Rivero Perez <erivero-@c2r2s5.42malaga.com>
2025-04-14 16:12:14 +0200

    ex02 done

---
+++ 0_DataEngineer/ex02/table.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+set -e
+
+TABLE_NAME="event_data"
+CSV_PATH="/csv_data/customer/data_2022_oct.csv"
+
+# Function to check if table already exists
+function table_exists() {
+  EXISTS=$(psql -U "$POSTGRES_USER" -d "$POSTGRES_DB" -tAc "SELECT EXISTS (SELECT FROM information_schema.tables WHERE table_name = '$TABLE_NAME');")
+  [ "$EXISTS" = "t" ]
+}
+
+if table_exists; then
+  echo "'$TABLE_NAME' table already exists."
+else
+  echo "Creating '$TABLE_NAME' table..."
+
+  # Create table
+  psql -U "$POSTGRES_USER" -d "$POSTGRES_DB" <<EOF
+    CREATE TABLE $TABLE_NAME (
+    event_time TIMESTAMP NOT NULL,
+    event_type VARCHAR(255),
+    product_id INTEGER,
+    price NUMERIC,
+    user_id BIGINT,
+    user_session UUID
+);
+EOF
+
+  # Load CSV
+  psql -U "$POSTGRES_USER" -d "$POSTGRES_DB" -c "\copy $TABLE_NAME FROM '$CSV_PATH' DELIMITER ',' CSV HEADER;"
+
+  echo "Table '$TABLE_NAME' succesfully created."
+fi
