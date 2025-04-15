06de7cde95332c3327dffc13c4d5a04f6ee35fae
erivero-p <elrivper@gmail.com>
2025-04-15 10:23:01 +0200

    ex04 done

---
+++ 0_DataEngineer/ex04/entrypoint.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -e
+
+# Call the original PostgreSQL entrypoint script to initialize the database
+docker-entrypoint.sh postgres &
+
+# Wait for PostgreSQL to be ready
+until pg_isready -U "$POSTGRES_USER" -d "$POSTGRES_DB"; do
+  echo "Waiting Postgress to be ready..."
+  sleep 2
+done
+
+# Run the custom script to create the table and load data
+echo "Creating table and loading data..."
+./items_table.sh
+
+# Keep the container running
+wait -n
---
+++ 0_DataEngineer/ex04/items_table.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+set -e
+
+TABLE_NAME="items"
+CSV_PATH="/csv_data/item/item.csv"
+
+# Function to check if table already exists
+function table_exists() {
+  EXISTS=$(psql -U "$POSTGRES_USER" -d "$POSTGRES_DB" -tAc "SELECT EXISTS (SELECT FROM information_schema.tables WHERE table_name = '$TABLE_NAME');")
+  [ "$EXISTS" = "t" ]
+}
+
+if table_exists; then
+  echo "'$TABLE_NAME' table already exists."
+else
+  echo "Creating '$TABLE_NAME' table..."
+
+  # Create table
+  psql -U "$POSTGRES_USER" -d "$POSTGRES_DB" <<EOF
+    CREATE TABLE $TABLE_NAME (
+    product_id INTEGER,
+    category_id BIGINT,
+    category_code VARCHAR(255),
+    brand VARCHAR(255)
+);
+EOF
+
+  # Load CSV
+  psql -U "$POSTGRES_USER" -d "$POSTGRES_DB" -c "\copy $TABLE_NAME FROM '$CSV_PATH' DELIMITER ',' CSV HEADER;"
+
+  echo "Table '$TABLE_NAME' succesfully created."
+fi
